e3af16b80c761fafd30cc7dc11beb39be2768ef6
Matt Logan <mlogan5212@gmail.com>
2021-02-08 20:50:31 -0600

    fixed divide by zero bug

--- bangazonapi/models/product.py
+++ bangazonapi/models/product.py
@@ -62,8 +62,11 @@ class Product(SafeDeleteModel):
         for rating in ratings:
             total_rating += rating.rating
 
-        avg = total_rating / len(ratings)
-        return avg
+        if len(ratings):
+            avg = total_rating / len(ratings)
+            return avg
+        else:
+            pass
 
     class Meta:
         verbose_name = ("product")
