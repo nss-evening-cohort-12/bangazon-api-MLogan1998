d4e8a482d8529086d2bec90f46f86ae4b2de07cd
Matt Logan <mlogan5212@gmail.com>
2021-02-13 10:49:56 -0600

    Generated report of expensive products

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import inexpensive_products
+from .views import inexpensive_products, expensive_products
 
 urlpatterns = [
-    path('reports/inexpensive', inexpensive_products)
+    path('reports/inexpensive', inexpensive_products),
+    path('reports/expensive', expensive_products)
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,2 +1,3 @@
 from .connection import Connection
 from .products.inexpensiveproducts import inexpensive_products
+from .products.expensiveproducts import expensive_products
