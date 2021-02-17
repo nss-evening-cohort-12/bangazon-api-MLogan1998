cf229dee4fa4a93452b12309ecad196e21f53513
Matt Logan <mlogan5212@gmail.com>
2021-02-16 18:53:40 -0600

    Created product rating view/serializer so products can be rated

--- bangazon/urls.py
+++ bangazon/urls.py
@@ -18,6 +18,7 @@ router.register(r'orders', Orders, 'order')
 router.register(r'cart', Cart, 'cart')
 router.register(r'paymenttypes', Payments, 'payment')
 router.register(r'profile', Profile, 'profile')
+router.register(r'rating', RatingVIewSet, 'productrating')
 
 
 # Wire up our API using automatic URL routing.
